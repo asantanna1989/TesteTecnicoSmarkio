c3efc9ce1b9e668b2c1a9b2317934fadc9987958
asantanna1989 <77083881+asantanna1989@users.noreply.github.com>
2021-05-03 12:17:11 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,20 +1,15 @@
 require 'capybara'
 require 'capybara/cucumber'
-require 'ffaker'
-require 'faker'
 require 'pry'
 require 'rspec'
-require 'screen-recorder'
 require 'selenium-webdriver'
 require 'yaml'
-require 'ffmpeg'
+
 
 EL = YAML.load_file('data/Elementos.yml')
 
 Capybara.configure do |config|
     config.default_max_wait_time = 60
-    config.app_host = "https://www.dlkmodas.com.br"
+    config.app_host = 'https://www.dlkmodas.com.br'
     config.default_driver = :selenium_chrome
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--incognito')
 end
