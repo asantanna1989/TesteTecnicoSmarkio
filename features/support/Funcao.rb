c93eb361cde3f116f8f09ff8adbd1ebf6af0dbca
asantanna1989 <77083881+asantanna1989@users.noreply.github.com>
2021-05-03 17:00:48 -0300

    Update Funcao.rb

--- features/support/Funcao.rb
+++ features/support/Funcao.rb
@@ -29,6 +29,21 @@ class Funcao
     save_screenshot("data/screenshots/#{nome}.png")
   end
 
+  def screen_recorder(cenario)
+    advanced = {
+    input:    {
+        framerate:  30,
+        video_size: '1366x768'
+    },
+    output:   {
+        r:       15, # Framerate
+    },
+    log:      'recorder.log',
+    loglevel: 'level+debug', # For FFmpeg
+    }
+    ScreenRecorder::Desktop.new(output: "data/video/#{cenario}.mp4", advanced: advanced)
+  end
+
   def seleciona_produto
     assert_selector(EL["produto_escolhido"], visible: true)
     find(EL["produto_escolhido"]).click
